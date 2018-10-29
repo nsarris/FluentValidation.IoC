7aa887c0e5be2b6bc542a313a6cbb4b5e5c4774b
nsarris <nsarris@gmail.com>
2018-10-29 22:39:49 +0200

    Clean up

---
+++ FluentValidation.IoC/IoCErrorMessageStringSource.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation.Validators;
+using System;
+
+namespace FluentValidation.IoC
+{
+    internal class IoCErrorMessageStringSource : Resources.IStringSource
+    {
+        private readonly string code;
+
+        public IoCErrorMessageStringSource(string code)
+        {
+            this.code = code;
+        }
+
+        public string GetString(IValidationContext context)
+        {
+            if (context == null)
+                return null;
+            else
+            {
+                if (context is ValidationContext validationContext)
+                {
+                    return validationContext.GetLiteralService().GetValidationErrorMessage(code);
+                }
+                else if(context is PropertyValidatorContext propertyValidationContext)
+                {
+                    return propertyValidationContext.ParentContext.GetLiteralService().GetValidationErrorMessage(code);
+                }
+                else
+                    return code;
+            }
+        }
+
+        public string ResourceName => null;
+
+        public Type ResourceType => null;
+    }
+}
--- FluentValidation.IoC/IoCPropertyNameStringSource.cs
+++ FluentValidation.IoC/IoCPropertyNameStringSource.cs
@@ -37,37 +37,4 @@ namespace FluentValidation.IoC
 
         public Type ResourceType => null;
     }
-
-    internal class IoCErrorMessageStringSource : Resources.IStringSource
-    {
-        private readonly string code;
-
-        public IoCErrorMessageStringSource(string code)
-        {
-            this.code = code;
-        }
-
-        public string GetString(IValidationContext context)
-        {
-            if (context == null)
-                return null;
-            else
-            {
-                if (context is ValidationContext validationContext)
-                {
-                    return validationContext.GetLiteralService().GetValidationErrorMessage(code);
-                }
-                else if(context is PropertyValidatorContext propertyValidationContext)
-                {
-                    return propertyValidationContext.ParentContext.GetLiteralService().GetValidationErrorMessage(code);
-                }
-                else
-                    return code;
-            }
-        }
-
-        public string ResourceName => null;
-
-        public Type ResourceType => null;
-    }
 }
