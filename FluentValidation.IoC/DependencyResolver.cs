5ba99073691e6c6224296a12c4df57d06395e29f
nsarris <nsarris@gmail.com>
2019-02-14 00:36:42 +0200

    Fix

--- FluentValidation.IoC/DependencyResolver.cs
+++ FluentValidation.IoC/DependencyResolver.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace FluentValidation.IoC
 {
-    public sealed class DependencyResolver : IDependencyResolver
+    internal sealed class DependencyResolver : IDependencyResolver, IValidatorFactory
     {
         private readonly IServiceProvider serviceProvider;
 
@@ -27,6 +27,33 @@ namespace FluentValidation.IoC
         public object GetService(Type serviceType)
         {
             return serviceProvider.GetService(serviceType);
+        }
+
+        public IValidator<T> GetValidator<T>()
+        {
+            return GetService<IValidator<T>>();
+        }
+
+        public IValidator GetValidator(Type type)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(type))
+                throw new InvalidOperationException($"Type {type.Name} does not implement IValidator");
+
+            return (IValidator)GetService(type);
+        }
+
+        public TValidator GetValidator<T, TValidator>()
+            where TValidator : IValidator<T>
+        {
+            return GetService<TValidator>();
+        }
+
+        public IValidator<T> GetValidator<T>(Type validatorType)
+        {
+            if (!typeof(IValidator<T>).IsAssignableFrom(validatorType))
+                throw new InvalidOperationException($"Type {validatorType.Name} does not implement IValidator<{typeof(T).Name}>");
+
+            return (IValidator<T>)GetService(validatorType);
         }
     }
 }
